6beb8dbcdd27ecfa1201ced95db23ba822f00c9e
Ernestc12 <chen.ernest99@gmail.com>
2016-09-01 20:54:39 -0700

    Math Library Day 2

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,15 @@
+
+public class DoMath {
+
+	public static void main(String[] args) {
+		Calculate.square(5);
+		Calculate.toImproperFrac(3, 1, 2);
+		Calculate.toMixedNum(7, 2);
+		System.out.println(Calculate.toMixedNum(7, 2));
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# EChenFall2016
